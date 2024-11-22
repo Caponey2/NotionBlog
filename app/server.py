b70c2168deeb42db1bde56648ed160feecbbc335
Tanner <tannerhebert20xx@gmail.com>
2024-11-22 14:48:21 -0800

    Updated server to only run flask in development

--- app/server.py
+++ app/server.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
 
-    return "hello world of githubactions"
+    return "hello world of gunicorn"
 
-app.run(host="0.0.0.0", port=3000, debug=True)
+#Flask development block
+if __name__ == "__main__":
+    
+    # Only used for local development
+    app.run(host="0.0.0.0", port=3000, debug=True)
